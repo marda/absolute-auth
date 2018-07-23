fceb50ef1881b9d7ee61e484c6dda2d1f87214be
Radek Semančík <radek.semancik@ogilvydevelopment.cz>
2018-07-23 13:45:22 +0200

    login

--- presenter/DefaultPresenter.php
+++ presenter/DefaultPresenter.php
@@ -2,47 +2,46 @@
 
 namespace Absolute\Module\Auth\Presenter;
 
+use Nette\Http\Response;
 use Nette\Application\Responses\JsonResponse;
 
 class DefaultPresenter extends AuthBasePresenter
 {
-  /** @var \Absolute\Module\User\Manager\UserManager @inject */
-  public $userManager;
 
-  public function startup() 
-  {
-    parent::startup();  
-  }
+    /** @var \Absolute\Module\User\Manager\UserManager @inject */
+    public $userManager;
 
-  public function renderDefault() 
-  {
-    switch($this->httpRequest->getMethod())
+    public function startup()
     {
-      case 'POST':
-        $data = $this->httpRequest->getRawBody();
-        $json = json_decode($data);
-        try 
-        {
-          $this->user->login($json->username, $json->password);
-          $this->user->setExpiration('+ 14 days', FALSE);
-          $user = $this->userManager->getByUsername($json->username);
-          $this->jsonResponse->payload = $user->toJson();
-          $this->httpResponse->setCode(Response::S200_OK);    
-        } 
-        catch (\Nette\Security\AuthenticationException $e) 
-        {
-          $this->jsonResponse->payload = ['message' => $e->getMessage()];
-          $this->httpResponse->setCode(Response::S401_UNAUTHORIZED);    
-        } 
-        break;
-      case 'OPTIONS':
-        $this->httpResponse->setCode(Response::S200_OK);  
-      default:
-        break;
+        parent::startup();
     }
-    $this->sendResponse(new JsonResponse( 
-      $this->jsonResponse->toJson(), "application/json;charset=utf-8"
-    ));
 
-  }
+    public function renderDefault()
+    {
+
+        switch ($this->httpRequest->getMethod()) {
+            case 'POST':                               
+                try
+                {
+                    $this->user->login($this->httpRequest->getPost('username'), $this->httpRequest->getPost('password'));
+                    $this->user->setExpiration('+ 14 days', FALSE);
+                    $user = $this->userManager->getByUsername($this->httpRequest->getPost('username'));                   
+                    $this->jsonResponse->payload = $user->toJson();
+                    $this->httpResponse->setCode(Response::S200_OK);
+                } catch (\Nette\Security\AuthenticationException $e)
+                {
+                    $this->jsonResponse->payload = ['message' => $e->getMessage()];
+                    $this->httpResponse->setCode(Response::S401_UNAUTHORIZED);
+                }
+                break;
+            case 'OPTIONS':
+                $this->httpResponse->setCode(Response::S200_OK);
+            default:
+                break;
+        }
+        $this->sendResponse(new JsonResponse(
+            $this->jsonResponse->toJson(), "application/json;charset=utf-8"
+        ));
+    }
+
 }
